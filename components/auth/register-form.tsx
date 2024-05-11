70ecb6add6a67a30f9c852d08b0d23782f69dc2d
Ling Vu <vumanhling@icloud.com>
2024-05-11 13:43:37 +0200

    Add register form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -5,7 +5,7 @@ import CardWrapper from "./card-wrapper";
 import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
@@ -13,25 +13,27 @@ import FormError from "../ui/form-error";
 import FormSuccess from "../ui/form-success";
 import { login } from "@/actions/login";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { register } from "@/actions/register";
 
-const LoginForm = () => {
+const RegisterForm = () => {
   const [error, setError] = useState<string>("");
   const [success, setSuccess] = useState<string>("");
   const [isPending, startTransition] = useTransition();
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (data: z.infer<typeof RegisterSchema>) => {
     startTransition(() => {
       setError("");
       setSuccess("");
 
-      login(data).then((response) => {
+      register(data).then((response) => {
         if (response.error) {
           setError(response.error);
         }
@@ -40,18 +42,31 @@ const LoginForm = () => {
         }
       });
     });
-  };
+  }
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
         <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
           <div className="space-y-4">
+          <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="John Doe" disabled={isPending} />
+                  </FormControl>
+                  <FormMessage>{form.formState.errors.name?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -59,7 +74,7 @@ const LoginForm = () => {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="john.doe@example.com" type="email" disabled={isPending} />
+                    <Input {...field} placeholder="john.doe@example.com" type="email" disabled={isPending}  />
                   </FormControl>
                   <FormMessage>{form.formState.errors.email?.message}</FormMessage>
                 </FormItem>
@@ -79,11 +94,10 @@ const LoginForm = () => {
               )}
             />
           </div>
-          {}
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
-            {isPending ? <AiOutlineLoading3Quarters className="mr-2 h-4 w-4 animate-spin" /> : "Login"}
+            {isPending ? <AiOutlineLoading3Quarters className="mr-2 h-4 w-4 animate-spin" /> : "Create an account"}
           </Button>
         </form>
       </Form>
@@ -91,4 +105,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
