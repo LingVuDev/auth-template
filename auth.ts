f205231a69667db81b4cf0c5b1b2adcf55d58b6d
Ling Vu <vumanhling@icloud.com>
2024-05-27 22:06:03 +0200

    Enable user roles with callbacks

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
@@ -6,8 +6,56 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import authConfig from "@/auth.config"
 import { db } from "@/lib/db"
+import { getUserById } from "./data/user"
+import { JWT } from "next-auth/jwt"
+
+type UserRole = 'ADMIN' | 'USER';
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's role. */
+      role: UserRole
+    } & DefaultSession["user"]
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** User role */
+    role: UserRole
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
